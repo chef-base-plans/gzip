1b4c0ccd75bfc95d222128c840d83e78c13cc853
Steven Marshall <SMarshall@chef.io>
2020-05-29 11:34:10 +0100

    [gzip] Modifying gzip controls
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/gzip_works.rb
+++ controls/gzip_works.rb
@@ -1,10 +1,23 @@
-gzip_test = input('gzip_test', value: '/bin/gzip -kv /hab/svc/gzip/hooks/run')
+gzip = input("gzip", value: "/bin/gzip")
+plan_origin = ENV["HAB_ORIGIN"]
+plan_name = input("plan_name", value: "gzip")
+plan_ident = "#{plan_origin}/#{plan_name}"
+hook_path = input("hook_path", value: "hooks/run")
 
-describe bash(gzip_test) do
-  its('stdout') { should match replaced }
-  its('exit_status') { should eq 0 }
+hab_pkg_path = command("hab pkg path #{plan_ident}")
+describe hab_pkg_path do
+  its("exit_status") { should eq 0 }
+  its("stdout") { should_not be_empty }
 end
 
-describe file('/hab/svc/gzip/hooks/run.gz') do
+run_hook = File.join(hab_pkg_path.stdout.strip, hook_path)
+
+#gzip verbose messages get sent to stderr so it is piped to stdout
+describe bash("#{gzip} -kv #{run_hook} 2>&1") do
+  its("exit_status") { should eq 0 }
+  its("stdout") { should match /created/ } 
+end
+
+describe file("#{run_hook}.gz") do
   it { should exist }
 end
